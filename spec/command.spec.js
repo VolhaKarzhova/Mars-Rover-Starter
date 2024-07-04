82eb70901190d33763b8eebf3aa3ad46316df9fa
Olga Karzhova <olgakorzhova@Olgas-Air.lan>
2024-07-04 15:55:03 -0500

    add command tests

--- spec/command.spec.js
+++ spec/command.spec.js
@@ -9,4 +9,12 @@ describe("Command class", function() {
     expect( function() { new Command();}).toThrow(new Error('Command type required.'));
   });
 
+  it("constructor sets command type", function() {
+    expect(new Command('MOVE').commandType).toBe('MOVE');
+  });
+
+  it("constructor sets a value passed in as the 2nd argument", function() {
+    expect(new Command('MOVE', 15).value).toBe(15);
+  });
+
 });
